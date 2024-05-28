bedf96d79024e24b14a8014028810ed2fbdd03b1
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2024-05-28 20:26:12 +0900

    week1 complete (Observer pattern)

---
+++ app/src/main/java/com/janob/mvvminstagram/login/InputNumberActivity.kt
@@ -0,0 +1,12 @@
+package com.janob.mvvminstagram.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.janob.mvvminstagram.R
+
+class InputNumberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_number)
+    }
+}
--- app/src/main/java/com/janob/mvvminstagram/login/LoginActivity.kt
+++ app/src/main/java/com/janob/mvvminstagram/login/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.janob.mvvminstagram.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.janob.mvvminstagram.R
@@ -9,13 +11,41 @@ import com.janob.mvvminstagram.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding : ActivityLoginBinding
-    lateinit var loginViewModel: LoginViewModel
+    val loginViewModel: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
-        loginViewModel = ViewModelProvider(this)[LoginViewModel::class.java]
         binding.viewModel = loginViewModel
+        binding.activity = this
         binding.lifecycleOwner = this
+
+        setObserve()
+    }
+
+    fun setObserve() {
+        loginViewModel.showInputNumberActivity.observe(this){
+            if(it){  //showInputNumberActivity가 true로 바꼈을 때
+                finish()
+                startActivity(Intent(this,InputNumberActivity::class.java))
+            }
+        }
+
+        loginViewModel.showFindIdActivity.observe(this){
+            if(it){  //showFindIdActivity가 true로 바꼈을 때
+                finish()
+                startActivity(Intent(this,FindIdActivity::class.java))
+            }
+        }
+    }
+
+    fun loginEmail(){
+        println("Email")
+        loginViewModel.showInputNumberActivity.value = true
+    }
+
+    fun findId(){
+        println("findId")
+        loginViewModel.showFindIdActivity.value = true
     }
 
 
