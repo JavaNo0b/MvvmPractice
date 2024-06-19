3222cf138f00ef06f5fea94854d349f664123ed0
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2024-06-19 17:29:04 +0900

    구글 로그인 구현

--- app/src/main/java/com/janob/mvvminstagram/login/LoginActivity.kt
+++ app/src/main/java/com/janob/mvvminstagram/login/LoginActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 import com.janob.mvvminstagram.R
 import com.janob.mvvminstagram.databinding.ActivityLoginBinding
@@ -45,6 +48,18 @@ class LoginActivity : AppCompatActivity() {
     fun findId(){
         println("findId")
         loginViewModel.showFindIdActivity.value = true
+    }
+
+    // 구글로그인이 성공한 결과값 받는 함수
+    // 이 함수를 뷰모델에 넣고 싶었으나 registerForActivityResult함수가 뷰모델에서는 사용할 수 없으므로 실패
+    var googleLoginResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+
+        val data = result.data
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        val account = task.getResult(ApiException::class.java)
+         //로그인한 사용자 정보를 암호화한 값
+        loginViewModel.firebaseAuthWithGoogle(account.idToken)
     }
 
 
--- app/src/main/java/com/janob/mvvminstagram/login/LoginViewModel.kt
+++ app/src/main/java/com/janob/mvvminstagram/login/LoginViewModel.kt
@@ -1,10 +1,18 @@
 package com.janob.mvvminstagram.login
 
+import android.app.Application
+import android.view.View
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.janob.mvvminstagram.R
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
     var id : MutableLiveData<String> = MutableLiveData("")
     var password : MutableLiveData<String> = MutableLiveData("")
 
@@ -13,10 +21,39 @@ class LoginViewModel : ViewModel() {
     var showInputNumberActivity : MutableLiveData<Boolean> = MutableLiveData(false)
     var showFindIdActivity : MutableLiveData<Boolean> = MutableLiveData(false)
 
+    val context = getApplication<Application>().applicationContext
+
+    var googleSignInClient : GoogleSignInClient
+
+    init {
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(context.getString(R.string.default_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(context, gso)
+    }
 
     fun loginWithSignupEmail(){
         println("Email")
         auth.createUserWithEmailAndPassword(id.value.toString(), password.value.toString()).addOnCompleteListener {
+            if(it.isSuccessful){
+                showInputNumberActivity.value = true
+            } else {
+                //아이디가 존재할 경우
+            }
+        }
+    }
+
+    fun loginGoogle(view : View){
+        var i = googleSignInClient.signInIntent
+        //googleSignInClient.signInIntent 와 googleLoginResult을 연결해주기 위해 (view.context as? LoginActivity)의 형태로 LoginActivity에 접근
+        (view.context as? LoginActivity)?.googleLoginResult?.launch(i)
+    }
+    fun firebaseAuthWithGoogle(idToken : String?){
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
             if(it.isSuccessful){
                 showInputNumberActivity.value = true
             } else {
